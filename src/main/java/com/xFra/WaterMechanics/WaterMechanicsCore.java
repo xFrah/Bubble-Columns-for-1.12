8d5fe5ceeea5478024f931ea05b75b235a2abc0c
xFrah <fdimonaco309@hotmail.com>
2026-08-14 09:44:45 +0200

    Fixed versioning

--- src/main/java/com/xFra/WaterMechanics/WaterMechanicsCore.java
+++ src/main/java/com/xFra/WaterMechanics/WaterMechanicsCore.java
@@ -15,7 +15,7 @@ public class WaterMechanicsCore
 	
     public static final String MODID = "113_water_mechanics";
     public static final String NAME = "Bubble Columns Backport";
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.0.7";
     private static Logger logger;
 
     @EventHandler
